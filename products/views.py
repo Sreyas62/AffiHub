a6ab61879ed17aadff47d3f6d9b3aacc1dd8e194
Sreyas62 <sreyasmpillai2049@gmail.com>
2025-06-30 23:52:46 +0530

    fix:fixed import in products

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from django.core.cache import cache
 from django.db.models import Q, Count, Sum
 from .models import Product, Category
